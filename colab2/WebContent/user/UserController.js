f60e5ebd5b4db482fe299586beeef0980667a62f
= <=>
2017-12-07 17:48:44 +0530

    new fornt end project colab2

---
+++ colab2/WebContent/user/UserController.js
@@ -0,0 +1,142 @@
+'use strict';
+
+app.controller('UserController', [
+		'$http',
+		'$cookieStore',
+		'$scope',
+		'UserService',
+		'$location',
+		'$rootScope',
+		'$window',
+		function($http, $cookieStore, $scope, UserService, $location, $rootScope, $window) {
+			console.log("UserController....")
+			var self = this;
+			self.user = {
+				errorCode: '',
+				errorMessage: '',
+				userId: '',
+				userPassword: '',
+				userName: '',
+				userRole: '',
+				userGender: '',
+				userDob: '',
+				userEmail: '',
+				userContactNumber: '',
+				userAddress: '',
+				userStatus: '',
+				userIsOnline: '',
+				userImage: '',
+				userDescription: ''
+				
+			};
+			self.users = [];
+
+			self.fetchAllUsers = function() {
+				console.log("--> UserController : calling fetchAllUsers method.");
+				UserService.fetchAllUsers().then(function(d) {
+					self.users = d;
+				}, function(errResponse) {
+					console.error('Error while fetching Users...');
+				});
+			};
+
+			self.createUser = function(user) {
+				console.log("--> UserController : calling createUser method.");
+				UserService.createUser(user).then(self.fetchAllUsers,
+						function(errResponse) {
+							console.error('Error while creating User...');
+						});
+			};
+
+			self.updateUser = function(user, id) {
+				console.log("--> UserController : calling updateUser method.");
+				UserService.updateUser(user, id).then(function(d) {
+					self.users = d;
+					$location.path('/myprofile');
+					}, function(errResponse) {
+						console.error('--> UserController : Error while updating User...');
+					});
+			};
+
+			self.authenticate = function(user) {
+				console.log("--> UserController : calling authenticate method.");
+				UserService.authenticate(user).then(function(d) {
+					self.user = d;
+					console.log("user.errorCode : "+self.user.errorCode);
+					if(self.user.errorCode == "404") {
+						alert("Invalid Credentials. Please try again.")
+						
+						self.user.userId = "";
+						self.user.userPassword = "";
+					} else {
+						console.log("Valid Credentials. Navigating to home page.");
+						$http.defaults.headers.common['Authorization'] = 'Basic' + $rootScope.currentUser;
+						$cookieStore.put('currentUser', $rootScope.currentUser);
+						$location.path('/');
+					}
+				}, 
+				function(errResponse) {
+					console.error('Error while authenticate User...');
+				});
+			};
+			
+			self.logout = function() {
+				console.log("--> UserController : calling logout method.");
+				$rootScope.currentUser = {};
+				$cookieStore.remove('currentUser');
+				UserService.logout();
+				console.log("-->UserController : User Logged out.");
+				
+				$window.location.reload();
+				$location.path('/');
+			}
+
+			self.deleteUser = function(id) {
+				console.log("--> UserController : calling deleteUser function.");
+				UserService.deleteUser(id).then(self.fetchAllUsers,
+						function(errResponse) {
+							console.error('Error while deleting User...');
+						});
+			};
+
+			self.fetchAllUsers();
+
+			self.login = function() {
+				{
+					console.log('login validation ??????????', self.user);
+					self.authenticate(self.user);
+				}
+			};
+
+			self.register = function() {
+				{
+					console.log("--> UserController : calling register() method.", self.user);
+					self.createUser(self.user);
+					console.log('Saving new user...');
+				}
+				$location.path('/login');
+				self.reset();
+			};
+			
+			self.reset = function() {
+				self.user = {
+						errorCode: '',
+						errorMessage: '',
+						userId: '',
+						userPassword: '',
+						userName: '',
+						userRole: '',
+						userGender: '',
+						userDob: '',
+						userEmail: '',
+						userContactNumber: '',
+						userAddress: '',
+						userStatus: '',
+						userIsOnline: '',
+						userImage: '',
+						userDescription: ''
+				};
+				$scope.myForm.$setPristine();
+			};
+
+		} ]);
---
+++ colab2/WebContent/user/UserService.js
@@ -0,0 +1,121 @@
+'use strict'
+
+app.factory('UserService', ['$http', '$q', '$rootScope',
+	function($http, $q, $rootScope) {
+		console.log('UserService...');
+
+		var BASE_URL = 'http://localhost:8080/collaboration'
+		return {
+			
+			fetchAllUsers : function() {
+				console.log("--> UserService : calling 'fetchAllUsers' method.");
+				return $http
+							.get(BASE_URL+'/users')
+							.then(function(response) {
+								return response.data;
+							},
+							function(errResponse) {
+								console.error('Error while fetching UserDetails...');
+								return $q.reject(errResponse);
+							});
+			},
+			
+			searchForFriends : function() {
+				console.log("--> UserService : calling 'fetchAllUsers' method.");
+				return $http
+							.get(BASE_URL+'/searchForFriends')
+							.then(function(response) {
+								return response.data;
+							},
+							function(errResponse) {
+								console.error('Error while fetching UserDetails...');
+								return $q.reject(errResponse);
+							});
+			},
+
+			createUser : function(user) {
+				console.log("--> UserService : calling 'createUser' method.");
+				return $http
+							.post(BASE_URL+'/user/', user)
+							.then(function(response) {
+								return response.data;
+							},
+							function(errResponse) {
+								console.error('Error while creating User...');
+								return $q.reject(errResponse);
+							});
+			},
+
+			updateUser : function(user, id) {
+				console.log("--> UserService : calling 'updateUser' method.");
+				return $http
+							.put(BASE_URL+'/user/'+id, user)
+							.then(function(response) {
+								return response.data;
+							},
+							function(errResponse) {
+								console.error('Error while updating User...');
+								return $q.reject(errResponse);
+							});
+			},
+
+			deleteUser : function(id) {
+				console.log("--> UserService : calling 'deleteUser' method.");
+				return $http
+							.delete(BASE_URL+'/user/'+id)
+							.then(function(response) {
+								return response.data;
+							},
+							function(errResponse) {
+								console.error('Error while deleting User...');
+								return $q.reject(errResponse);
+							});
+			},
+
+			authenticate : function(user) {
+				console.log("--> UserService : calling 'authenticate' method.");
+				return $http
+							.post(BASE_URL+'/user/login', user)
+							.then(function(response) {
+								if (response.data.errorMessage!="") {
+									$rootScope.currentUser = {
+											userId: response.data.userId,
+											userPassword: response.data.userPassword,
+											userName: response.data.userName,
+											userRole: response.data.userRole,
+											userGender: response.data.userGender,
+											userDob: response.data.userDob,
+											userEmail: response.data.userEmail,
+											userContactNumber: response.data.userContactNumber,
+											userAddress: response.data.userAddress,
+											userStatus: response.data.userStatus,
+											userIsOnline: response.data.userIsOnline,
+											userImage: response.data.userImage,
+											userDescription: response.data.userDescription
+											
+									};
+								}
+								return response.data;
+							},
+							function(errResponse) {
+								console.error('Error while authenticate User...');
+								return $q.reject(errResponse);
+							});
+			},
+			
+			logout: function() {
+				console.log("--> UserService : calling 'logout' method.");
+				return $http
+							.get(BASE_URL+'/user/logout')
+							.then(function(response) {
+								return response.data;
+							},
+							function(errResponse) {
+								console.error('Error while logging out.');
+								return $q.reject(errResponse);
+							}
+						);
+			}
+		};
+
+	}]);
